cfb3780e8a1674961e43a48468fbb915fcf67b3e
Choi Hwi bin <chlgnlqls3@nate.com>
2024-01-11 01:07:12 +0900

    #40-카카오톡 로그인 기능 구현

---
+++ springboot-board-project/src/main/java/com/hwibin/boardproject/service/UserAccountService.java
@@ -0,0 +1,31 @@
+package com.hwibin.boardproject.service;
+
+import com.hwibin.boardproject.domain.UserAccount;
+import com.hwibin.boardproject.dto.UserAccountDto;
+import com.hwibin.boardproject.repository.UserAccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class UserAccountService {
+
+    private final UserAccountRepository userAccountRepository;
+
+    @Transactional(readOnly = true)
+    public Optional<UserAccountDto> searchUser(String username) {
+        return userAccountRepository.findById(username)
+                .map(UserAccountDto::from);
+    }
+
+    public UserAccountDto saveUser(String username, String password, String email, String nickname, String memo) {
+        return UserAccountDto.from(
+                userAccountRepository.save(UserAccount.of(username, password, email, nickname, memo, username))
+        );
+    }
+
+}
--- springboot-board-project/src/test/java/com/hwibin/boardproject/controller/MainControllerTest.java
+++ springboot-board-project/src/test/java/com/hwibin/boardproject/controller/MainControllerTest.java
@@ -1,5 +1,6 @@
 package com.hwibin.boardproject.controller;
 
+import com.hwibin.boardproject.config.TestSecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
