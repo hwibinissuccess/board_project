a2058ed7cba246bbaca4b0d9b71c1982a5844614
Choi Hwi bin <chlgnlqls3@nate.com>
2024-01-10 17:22:34 +0900

    #38-댓글기능고도화-n차댓글

--- springboot-board-project/src/main/java/com/hwibin/boardproject/dto/request/ArticleCommentRequest.java
+++ springboot-board-project/src/main/java/com/hwibin/boardproject/dto/request/ArticleCommentRequest.java
@@ -17,7 +17,7 @@ public record ArticleCommentRequest(
         return new ArticleCommentRequest(articleId, parentCommentId, content);
     }
 
-    public com.hwibin.boardproject.ArticleCommentDto toDto(UserAccountDto userAccountDto) {
+    public ArticleCommentDto toDto(UserAccountDto userAccountDto) {
         return ArticleCommentDto.of(
                 articleId,
                 userAccountDto,
